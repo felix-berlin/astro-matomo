1bb90a84ddc6162269f8df70dc1d939e7617e370
Felix Scholze <felix@webshaped.de>
2023-08-22 21:48:30 +0200

    fix: color the disable massage

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,46 @@
-import type { AstroIntegration } from 'astro';
+import type { AstroIntegration } from "astro";
 
-export type MatomoOptions = | {
-  enabled: boolean,
-  host: string,
-  siteId: number,
-  trackerUrl?: string,
-  srcUrl?: string,
-  heartBeatTimer?: number,
-  setCookieDomain?: string,
-  disableCookies?: boolean,
-  preconnect?: boolean,
-  debug?: boolean
-} | undefined;
+export type MatomoOptions =
+  | {
+      enabled: boolean;
+      host: string;
+      siteId: number;
+      trackerUrl?: string;
+      srcUrl?: string;
+      heartBeatTimer?: number;
+      setCookieDomain?: string;
+      disableCookies?: boolean;
+      preconnect?: boolean;
+      debug?: boolean;
+    }
+  | undefined;
 
 export default function createPlugin(options: MatomoOptions): AstroIntegration {
-  let script = `import {initMatomo, preconnectMatomo} from 'astro-matomo/matomo.ts'; initMatomo(${JSON.stringify(options)});`
+  let script = `import {initMatomo, preconnectMatomo} from 'astro-matomo/matomo.ts'; initMatomo(${JSON.stringify(
+    options
+  )});`;
 
-  if (options?.preconnect) script += `preconnectMatomo(${JSON.stringify(options)});`
+  if (options?.preconnect)
+    script += `preconnectMatomo(${JSON.stringify(options)});`;
 
   if (!options?.enabled) {
-    console.warn('Matomo is disabled!');
+    console.info(
+      "\x1b[44m",
+      "Matomo",
+      "\x1b[0m",
+      "\x1b[34m",
+      "Is disabled.",
+      "\x1b[0m"
+    );
+
     script = "";
   }
 
   return {
-    name: 'astro-matomo',
+    name: "astro-matomo",
     hooks: {
-      'astro:config:setup': async ({ injectScript }) => {
-        injectScript(
-          "page",
-          script
-        )
+      "astro:config:setup": async ({ injectScript }) => {
+        injectScript("page", script);
       },
     },
   };
