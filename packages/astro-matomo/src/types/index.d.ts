0a222f1892998ec873fce486205b7ec51cf2f9d6
Felix Scholze <felix@webshaped.de>
2026-07-06 10:55:41 +0200

    fix: type usage

--- packages/astro-matomo/src/types/index.d.ts
+++ packages/astro-matomo/src/types/index.d.ts
@@ -3,6 +3,14 @@ export {};
 declare global {
   interface Window {
     _paq: Array<[string, ...unknown[]]>;
-    _mtm: Array<[string, ...unknown[]]>;
+import type { MatomoCommand } from "../index.ts";
+
+export {};
+
+declare global {
+  interface Window {
+    _paq: Array<MatomoCommand | (() => void)>;
+    _mtm: Array<Record<string, unknown> | MatomoCommand>;
   }
 }
+
